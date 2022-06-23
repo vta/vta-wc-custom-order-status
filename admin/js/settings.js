474220d65db13597025599116270a904e0f46f82
James <jpham@guidelinecentral.com>
2022-06-23 13:24:16 -0700

    WIP - sortable array working

--- admin/js/settings.js
+++ admin/js/settings.js
@@ -2,6 +2,26 @@
  * @title VTA Custom Order Status - Settings JS
  * @description script for settings page
  */
+
+function returnOrder() {
+
+}
+
 (function ($) {
-    $('#sortable').sortable();
+
+    const sortable = $('#statuses-sortable');
+    const orderStatusArr = () => {
+        const children = sortable.children('li')
+        return children.map(function () {
+            return this.id;
+        });
+    }
+
+    sortable.sortable({
+        // update: function (event, ui) {
+        //     // console.log('change', event, ui);
+        //     const children = sortable.children('li')
+        // }
+    });
+
 })(jQuery);
--- admin/views/partials/settings/order-field.php
+++ admin/views/partials/settings/order-field.php
@@ -6,18 +6,12 @@
  */
 
 $order_statuses = wc_get_order_statuses();
-
-foreach( $order_statuses as $order_status ) {
-
-}
 ?>
 
-<ul id="sortable">
-    <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 1</li>
-    <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 2</li>
-    <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 3</li>
-    <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 4</li>
-    <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 5</li>
-    <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 6</li>
-    <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 7</li>
+<ul id="statuses-sortable">
+    <?php foreach ( $order_statuses as $status_key => $status_name ): ?>
+
+        <li class="ui-state-default" id="<?php echo $status_key; ?>"><?php echo $status_name; ?></li>
+
+    <?php endforeach; ?>
 </ul>

9402d94f6a9ddd77f033099107e0b47ed7b1597f
James <jamespham93@yahoo.com>
2022-08-10 11:17:41 -0700

    Constructor Exceptions for VTACustomOrderStatus

--- models/VTACustomOrderStatus.php
+++ models/VTACustomOrderStatus.php
@@ -8,12 +8,30 @@ class VTACustomOrderStatus {
 
     const POST_TYPE = VTA_COS_CPT;
 
-    private $post;
+    private WP_Post $post;
 
     /**
      * Encapsulates WP_Post in custom
      * @param WP_Post|int $post
+     * @throws Exception
      */
     public function __construct( $post ) {
+        $wp_post = get_post($post);
+
+        // Invalid constructor parameter Exceptions...
+        if ( !$wp_post instanceof WP_Post ) {
+            $post_arg_json = json_encode(is_int($post) ? $post : (array)$post, JSON_PRETTY_PRINT);
+            $err_msg       = "VTACustomOrderStatus::__construct() error - Invalid post. No post found for \"$post_arg_json\"";
+            throw new Exception($err_msg);
+
+        } elseif ( $post->post_type !== self::POST_TYPE ) {
+            $post_type = self::POST_TYPE;
+            $err_msg   = "VTACustomOrderStatus::__construct() error - Post is not of type $post_type. Post #{$post->ID} is of type {$post->post_type}";
+            throw new Exception($err_msg);
+        }
+    }
+
+    public function get_post(): WP_Post {
+        return $this->post;
     }
 }

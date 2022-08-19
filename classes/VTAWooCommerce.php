9efcdced1af249d56f8868cb6a9e8326173508ca
James Pham <jamespham93@yahoo.com>
2022-08-19 11:34:34 -0700

    Working Pending Orders viewwq

--- classes/VTAWooCommerce.php
+++ classes/VTAWooCommerce.php
@@ -30,6 +30,9 @@ class VTAWooCommerce {
     /** @var VTACustomOrderStatus[] */
     private array $vta_cos;
 
+    /** @var string[] */
+    private array $pending_status_keys;
+
     /**
      * @param VTACosSettings $settings
      * @param string $plugin_name
@@ -39,13 +42,14 @@ class VTAWooCommerce {
         $this->plugin_name    = $plugin_name;
         $this->plugin_version = $plugin_version;
 
-        $this->settings = $settings;
-        $this->vta_cos = $this->get_cos();
+        $this->settings            = $settings;
+        $this->vta_cos             = $this->get_cos();
+        $this->pending_status_keys = $this->get_pending_cos_keys(true);
 
         /**
          * Need to run this hook first before other filter methods are ran in the Orders page
          */
-//        add_action('pre_get_posts', [ $this, 'query_include_deprecated' ], 10, 1);
+        add_action('pre_get_posts', [ $this, 'query_include_deprecated' ], 10, 1);
         add_action('pre_get_posts', [ $this, 'query_pending_orders' ], 10, 1);
         add_filter('woocommerce_register_shop_order_post_statuses', [ $this, 'append_vta_cos' ], 10, 1);
         add_filter('wc_order_statuses', [ $this, 'register_vta_cos' ], 10, 1);
@@ -112,9 +116,12 @@ class VTAWooCommerce {
         $all_html = $views['all'];
         unset($views['all']);
 
+        [ 'query_params' => $query_params ] = get_query_params();
+
         // pending orders
         $html = sprintf(
-            '<a href="edit.php?post_status=pending-orders&#038;post_type=shop_order">Pending Orders <span class="count">(%d)</span></a>',
+            '<a href="edit.php?post_status=pending-orders&#038;post_type=shop_order" %s>Pending Orders <span class="count">(%d)</span></a>',
+                ($query_params['post_status'] ?? null) === 'pending-orders' ? 'class="active"' : '',
             $this->get_pending_orders_count()
         );
 
@@ -126,7 +133,7 @@ class VTAWooCommerce {
         $views = [ 'all' => $all_html ] + $views;
 
         // remove all empty values
-        return array_filter($views, fn( $link ) => is_string($link));
+        return array_filter($views, fn ( $link ) => is_string($link));
     }
 
     // QUERY MODIFICATIONS //
@@ -140,7 +147,7 @@ class VTAWooCommerce {
         [ 'path' => $path, 'query_params' => $query_params ] = get_query_params();
 
         // Orders page for all account
-        $is_my_account = (bool)preg_match('/my-account\/orders/', $path);
+        $is_my_account = (bool) preg_match('/my-account\/orders/', $path);
 
         // list table page for all WC orders
         $is_all_orders  = count($query_params) === 1;
@@ -152,25 +159,27 @@ class VTAWooCommerce {
          * - post type is "shop_order"
          */
         if ( $is_my_account || $is_edit_orders ) {
-            error_log('should not run');
             $wp_query->set('post_status', 'any');
         }
     }
 
+    /**
+     * Intercepts Query for Pending Orders view & returns the correct query for post statuses
+     * @param WP_Query $wp_query
+     * @return void
+     */
     public function query_pending_orders( WP_Query $wp_query ): void {
         [ 'path' => $path, 'query_params' => $query_params ] = get_query_params();
 
         $is_edit_orders = preg_match('/edit\.php/', $path) && in_array($this->shop_post_type, $query_params);
 
-//        $debug = $wp_query->get('post_status');
-
         if (
             is_admin() && $is_edit_orders &&
-            in_array('pending-orders', $query_params)
+            in_array('pending-orders', $query_params) &&
+            $wp_query->get('post_type') === $this->shop_post_type
         ) {
-            error_log('filtering pending orders...');
             remove_action('pre_get_posts', [ $this, 'query_pending_orders' ], 11);
-            $wp_query->set('post_status', $this->get_pending_cos_keys());
+            $wp_query->set('post_status', $this->pending_status_keys);
             add_action('pre_get_posts', [ $this, 'query_pending_orders' ], 11, 1);
         }
     }
@@ -189,7 +198,7 @@ class VTAWooCommerce {
                 'posts_per_page' => -1
             ]);
             $order_statuses = $wp_query->posts;
-            return array_map(fn( $post ) => new VTACustomOrderStatus($post), $order_statuses);
+            return array_map(fn ( $post ) => new VTACustomOrderStatus($post), $order_statuses);
 
         } catch ( Exception $e ) {
             error_log("VTAWooCommerce::get_cos() error. Could not convert post status VTA Custom Order Statuses. - $e");
@@ -206,7 +215,7 @@ class VTAWooCommerce {
         $arrangement_ids = $this->settings->get_arrangement();
 
         try {
-            $arrangement_cos_keys = array_map(fn( int $post_id ) => (new VTACustomOrderStatus($post_id))->get_cos_key(true), $arrangement_ids);
+            $arrangement_cos_keys = array_map(fn ( int $post_id ) => (new VTACustomOrderStatus($post_id))->get_cos_key(true), $arrangement_ids);
             return array_replace(array_flip($arrangement_cos_keys), $order_statuses);
         } catch ( Exception $e ) {
             error_log("VTAWooCommerce::sort_order_status error. Could not sort custom order statuses - $e");
@@ -225,7 +234,7 @@ class VTAWooCommerce {
         // manually filter orders since pre_get_posts interferes with count
         $filtered_orders = array_filter(
             $orders,
-            fn( WC_Order $wc_order ) => in_array($wc_order->get_status(), $non_reorderable_cos)
+            fn ( WC_Order $wc_order ) => in_array($wc_order->get_status(), $non_reorderable_cos)
         );
 
         return count($filtered_orders);
@@ -248,10 +257,10 @@ class VTAWooCommerce {
         $vta_cos      = $this->vta_cos;
         $filtered_cos = array_filter(
             $vta_cos,
-            fn( VTACustomOrderStatus $order_status ) => !$order_status->get_cos_reorderable() && !in_array($order_status->get_cos_key(), $other_non_pending_statuses)
+            fn ( VTACustomOrderStatus $order_status ) => !$order_status->get_cos_reorderable() && !in_array($order_status->get_cos_key(), $other_non_pending_statuses)
         );
 
-        return array_values(array_map(fn( VTACustomOrderStatus $order_status ) => $order_status->get_cos_key($with_prefix), $filtered_cos));
+        return array_values(array_map(fn ( VTACustomOrderStatus $order_status ) => $order_status->get_cos_key($with_prefix), $filtered_cos));
     }
 
 }
